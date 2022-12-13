ac43dd96b378af5a14fb7552a2fe3ef56a151f80
Alan Martins <alan-martins07@hotmail.com>
2022-12-13 12:56:28 -0300

    first commit. added files o/

---
+++ assets/js/auth.js
@@ -0,0 +1,106 @@
+    var stateKey = 'spotify_auth_state';
+
+    let displayName = document.getElementById('nameDisplayClient');
+    let loginButton = document.getElementById('login-button');
+
+    let table = document.querySelector('.table-top-tracks');
+
+    function getHashParams() {
+      var hashParams = {};
+      var e, r = /([^&;=]+)=?([^&;]*)/g,
+          q = window.location.hash.substring(1);
+      while ( e = r.exec(q)) {
+         hashParams[e[1]] = decodeURIComponent(e[2]);
+      }
+      return hashParams;
+    }
+
+    function generateRandomString(length) {
+      var text = '';
+      var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+      for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+      }
+      return text;
+    };
+
+    var params = getHashParams();
+
+    var access_token = params.access_token,
+        state = params.state,
+        storedState = localStorage.getItem(stateKey);
+
+    if (access_token && (state == null || state !== storedState)) {
+      alert('Ocorreu um erro durante a autenticação. :(');
+    } else {
+      localStorage.removeItem(stateKey);
+      if (access_token) {
+        console.log("sucesso, token: "+access_token);
+        alterToLockNewRequests();
+      }
+
+      loginButton.addEventListener('click', function() {
+
+        var client_id = 'e5e2cfc631ff4d00bb31f9cd5da6befe'; 
+        var redirect_uri = 'http://localhost:5500/';
+
+        var state = generateRandomString(16);
+
+        localStorage.setItem(stateKey, state);
+        var scope = 'user-read-private user-read-email user-top-read';
+
+        var url = 'https://accounts.spotify.com/authorize';
+        url += '?response_type=token';
+        url += '&client_id=' + encodeURIComponent(client_id);
+        url += '&scope=' + encodeURIComponent(scope);
+        url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+        url += '&state=' + encodeURIComponent(state);
+
+        window.location = url;
+      }, false);
+
+      function alterToLockNewRequests(){
+        document.getElementById('login-button').style.display = 'none';
+        document.querySelector('.styled-table').style.display = 'block';
+        handleDataTable();
+      }
+
+    function handleDataTable(){
+        if(access_token){
+            fetch('https://api.spotify.com/v1/me/top/tracks?time_range=medium_term&limit=5&offset=1', {
+                method: 'GET', headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + access_token
+                }
+            })
+            .then((response) => {
+                (response.json().then(
+                    (data) => {
+                        let topTracks = data.items;
+                        topTracks.forEach(item => {
+                            let nameAlbum = item.album.name;
+                            let trackName = item.name;
+                            let artist = item.album.artists[0].name;
+                            let duration = (item.duration_ms / 60000).toFixed(2).replace('.', ':');
+                            let tableRow = document.createElement("tr");
+
+                                tableRow.innerHTML = `                        
+                                <td>${data.items.indexOf(item) + 1}</td>
+                                <td>${trackName}</td>
+                                <td>${nameAlbum}</td>
+                                <td>${artist}</td>
+                                <td>${duration}</td>`;
+
+                            console.log(trackName, nameAlbum, artist, duration);
+                                table.appendChild(tableRow);
+
+                        });
+                     }
+                    ));
+                });
+                
+            }
+        }
+}
